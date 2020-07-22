8745dd25da29c32946e6863d92ae68cd02a04d34
kathryn0908 <kathrynlael@gmail.com>
2020-07-22 12:48:03 -0600

    updated donations styling and created a reduce function to add up all donations per charity

--- src/components/DonationCard.js
+++ src/components/DonationCard.js
@@ -3,22 +3,41 @@ import React from 'react';
 export default function DonationCard(props){
 const displayAmount = () => {
     return props.favorite.charity.donations.map(donation => {
-        console.log(donation.created_at.slice(0,10))
+        // console.log(donation.created_at.slice(0,10))
         return (
-            <p>Donated amount: ${donation.amount} on {donation.created_at.slice(0,10)}</p>
+            <p>Donated ${donation.amount} on {donation.created_at.slice(0,10)}</p>
         )
     })
 }
 
-// const displayTotal = () => {
-//     const amount = 
-// }
+const displayTotal = () => {
+    const donations = props.favorite.charity.donations
+    const result = Object.keys(donations).map(function (key){
+        return donations[key].amount;
+    })
+    
+    var sum = result.reduce(function(a,b){
+        return a + b;
+    }, 0)
+
+    if(sum == 0){
+        return <p className='no-donation-header'>You have not donated yet!</p>
+    }
+    else{
+        return (
+        <>   
+            <h1 className='total-header'>Total Donated</h1>
+            <p>${sum}.00</p>
+        </>
+        )
+    }
+}
 
 return(
-<>
-    <h1>{props.favorite.charity.charity_name}</h1>
+<div className='donations-card'>
+    <h1 className='donations-header'>{props.favorite.charity.charity_name}</h1>
     {displayAmount()}
-    {/* {displayTotal()} */}
-</>
+    {displayTotal()}
+</div>
 )
 }
